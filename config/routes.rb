12e90b2032f89c77f4a48d99ae9f0742750c9edd
Evan Lloyd <eulloyd@gmail.com>
2013-12-28 14:03:17 +0900

    Makes routes for Tools and views for Tools#form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 NgslDictionary::Application.routes.draw do
   get "entries/index"
   get "entries/show"
+  get "tools/form"
+  get "tools/report"
+  post "tools/report"
   resources :entries
   root "entries#home"
 end
