e5df2e7bdc121f506f333d22bbf7f6aed1aef4cd
Evan Lloyd <eulloyd@gmail.com>
2013-12-28 09:18:24 +0900

    Genereate empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NgslDictionary::Application.config.secret_key_base = '2815e36a9958ca9401feaf2ea2e9bb361b2fe92cbc1b31895e9d178104670fb6b72bc1fbb51abf3f87ff27dc8271981d158a9a4b91d7266c959ec5aa7aa45cf8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NgslDictionary::Application.config.session_store :cookie_store, key: '_ngsl-dictionary_session'
